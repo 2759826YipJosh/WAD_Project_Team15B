dcb8d551cd19d2a9ec8fd64782b79180c14d90c5
Josh Yip <2759826Y@student.gla.ac.uk>
2024-03-07 13:24:19 +0000

    making the backbone of some of the register stuff (running into problems though VIEW COMMENTS with -J)

--- GameDB/admin.py
+++ GameDB/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+#CODE RIPPED FROM TANGO_WITH_DJANGO - J
+
+from django.contrib import admin
+from GameDB.models import UserProfile
+
+# Register your models here.
+
+from django.contrib import admin
+from GameDB.models import Category, Page
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
+    
+...
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    # Update the registration to include this customised interface
+
+
+
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
